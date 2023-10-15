d56aaed08bc9cc677837263585eb1a31123cec06
quewon <36914642+quewon@users.noreply.github.com>
2023-10-15 21:23:40 +0900

    working version

---
+++ arduinocontroller/app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+http.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+// johnny five
+
+let five = require("johnny-five");
+let board = new five.Board();
+let connection;
+
+const P1 = 9;
+const P2 = 10;
+const P3 = 11;
+const P4 = 12;
+
+// var runStepper = false;
+var runStepper = true;
+
+const delayAmount = 3;
+var step = 0;
+
+board.on("ready", function() {
+  this.pinMode(P1, five.Pin.OUTPUT);
+  this.pinMode(P2, five.Pin.OUTPUT);
+  this.pinMode(P3, five.Pin.OUTPUT);
+  this.pinMode(P4, five.Pin.OUTPUT);
+
+  this.loop(4, () => {
+    updateStepper();
+
+    console.log(runStepper);
+  });
+
+  io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('data', (data) => {
+      // runStepper = data > 0;
+    });
+  });
+});
+
+function updateStepper() {
+  if (!runStepper) return;
+
+  switch (step) {
+    case 0:
+      board.digitalWrite(P1, 1);
+      board.digitalWrite(P2, 0);
+      board.digitalWrite(P3, 0);
+      board.digitalWrite(P4, 0);
+      break;
+    case 1:
+      board.digitalWrite(P1, 0);
+      board.digitalWrite(P2, 1);
+      board.digitalWrite(P3, 0);
+      board.digitalWrite(P4, 0);
+      break;
+    case 2:
+      board.digitalWrite(P1, 0);
+      board.digitalWrite(P2, 0);
+      board.digitalWrite(P3, 1);
+      board.digitalWrite(P4, 0);
+      break;
+    case 3:
+      board.digitalWrite(P1, 0);
+      board.digitalWrite(P2, 0);
+      board.digitalWrite(P3, 0);
+      board.digitalWrite(P4, 1);
+      break;
+  }
+  step++;
+  if (step > 3) step = 0;
+}
---
+++ arduinocontroller/public/face-landmarker.js
@@ -0,0 +1,74 @@
+import vision from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3";
+const { FaceLandmarker, FilesetResolver } = vision;
+
+let faceLandmarker;
+let runningMode = "VIDEO";
+let webcamRunning = false;
+let lastVideoTime;
+let results;
+
+const webcam = document.querySelector("video");
+const enableWebcamButton = document.querySelector("button");
+
+createFaceLandmarker();
+
+async function createFaceLandmarker() {
+  enableWebcamButton.disabled = true;
+
+  const filesetResolver = await FilesetResolver.forVisionTasks(
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
+  );
+  faceLandmarker = await FaceLandmarker.createFromOptions(filesetResolver, {
+    baseOptions: {
+      modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+      delegate: "GPU"
+    },
+    outputFaceBlendshapes: false,
+    runningMode: "VIDEO",
+    numFaces: 1,
+  });
+
+  enableWebcamButton.onclick = enableWebcam;
+  enableWebcamButton.disabled = false;
+}
+
+function enableWebcam(event) {
+  const constraints = {
+    video: true
+  };
+
+  navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    webcam.srcObject = stream;
+    webcam.addEventListener("loadeddata", function() {
+      initData();
+
+      predictWebcam();
+    });
+  });
+}
+
+async function predictWebcam() {
+  let startTimeMs = performance.now();
+  if (lastVideoTime !== webcam.currentTime) {
+    lastVideoTime = webcam.currentTime;
+    results = faceLandmarker.detectForVideo(webcam, startTimeMs);
+  }
+
+  document.body.classList.remove("face-detected");
+  if (results.faceLandmarks) {
+    sendData(results.faceLandmarks.length);
+    console.log("faces detected: "+results.faceLandmarks.length);
+    if (results.faceLandmarks.length > 0) document.body.classList.add("face-detected");
+  }
+
+  window.requestAnimationFrame(predictWebcam);
+}
+
+const socket = io();
+function initData() {
+
+}
+
+function sendData(data) {
+  socket.emit("data", data);
+}
